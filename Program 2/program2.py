342a1279edb72113189eedf0d8d0e8cf7dcf5eca
atlaska826 <131003995+atlaska826@users.noreply.github.com>
2023-09-19 14:39:48 -0500

    Updated program files.

--- Program 2/program2.py
+++ Program 2/program2.py
@@ -1,7 +1,13 @@
 """
 Name: Atlas Mallams
 Class Section: 0018
-Program 1 Code
+Program 2 Code
 Creation Date: September 16, 2023
 Due Date: September 24, 2023
 """
+
+'''FUNCTIONS'''
+# Functions here
+
+'''MAIN CODE'''
+# Main code here
--- Template/template_code.py
+++ Template/template_code.py
@@ -5,3 +5,17 @@ Program # ItemType
 Creation Date: Month #, 2023
 Due Date: Month #, 2023
 """
+
+'''FUNCTIONS'''
+# Functions
+
+'''MAIN CODE'''
+# Main code
+
+"""
+string = 'Hello'
+for i in range(-1,-len(string) - 1, -1):
+    print(f'{i} {string[i]}')
+
+print(chr(65))
+"""
