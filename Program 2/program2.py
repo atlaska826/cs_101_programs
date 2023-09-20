f41ca35584931eede117a4b61cd21f7f9aab5770
atlaska826 <131003995+atlaska826@users.noreply.github.com>
2023-09-19 23:27:45 -0500

    Worked on program 2

--- Program 2/program2.py
+++ Program 2/program2.py
@@ -2,12 +2,103 @@
 Name: Atlas Mallams
 Class Section: 0018
 Program 2 Code
-Creation Date: September 16, 2023
+Creation Date: September 19, 2023
 Due Date: September 24, 2023
 """
 
+'''PROGRAM VARIABLES'''
+add_count = 0
+subtract_count = 0
+multiply_count = 0
+divide_count = 0
+
 '''FUNCTIONS'''
-# Functions here
+def print_display_menu():
+    print('\n1. Addition')
+    print('2. Subtraction')
+    print('3. Multiplication')
+    print('4. Division')
+    print('5. Exit\n')
+
+def print_total_operations():
+    operation_count = add_count + subtract_count + multiply_count + divide_count
+    print(f'\nYou had {operation_count} arithmetic operations as below:')
+    if add_count > 0:
+        print(f'"+" : {add_count}')
+    if subtract_count > 0:
+        print(f'"-" : {subtract_count}')
+    if multiply_count > 0:
+        print(f'"*" : {multiply_count}')
+    if divide_count > 0:
+        print(f'"/" : {divide_count}')
+    if add_count == subtract_count == multiply_count == divide_count == 0:
+        print(f'No operations.')
+
+    print('\nExiting the calculator. Goodbye!')
+def ask_new_calculation():
+    perform_new_calc = input('\nDo you want to perform another calculation? (y/n): ').lower()
+
+    while perform_new_calc != 'y' and perform_new_calc != 'n':
+        perform_new_calc = input('Invalid choice, please select (y/n): ')
+    if perform_new_calc == 'y':
+        return True
+    else:
+        return False
+
+def get_user_operation():
+    print_display_menu()
+    answer_choice = int(input('Please select an operation (1-5): '))
+    while not(0 < answer_choice < 6):
+        print('Invalid choice. Please select a valid operation.\n')
+        answer_choice = int(input('Please select an operation (1-5): '))
+    return answer_choice
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    return num1 / num2
 
 '''MAIN CODE'''
-# Main code here
+print('Welcome to the Simple Calculator!')
+
+while True:
+    operation = get_user_operation()
+
+    if operation == 1:
+        add_count += 1
+        num1 = int(input('Enter the first number: '))
+        num2 = int(input('Enter the second number: '))
+        print(f'Result: {num1:.1f} + {num2:.1f} = {add(num1, num2):.2f}')
+    elif operation == 2:
+        subtract_count += 1
+        num1 = int(input('Enter the first number: '))
+        num2 = int(input('Enter the second number: '))
+        print(f'Result: {num1:.1f} - {num2:.1f} = {subtract(num1, num2):.2f}')
+    elif operation == 3:
+        multiply_count += 1
+        num1 = int(input('Enter the first number: '))
+        num2 = int(input('Enter the second number: '))
+        print(f'Result: {num1:.1f} * {num2:.1f} = {multiply(num1, num2):.2f}')
+    elif operation == 4:
+        divide_count += 1
+        num1 = int(input('Enter the first number: '))
+        num2 = int(input('Enter the second number: '))
+        while num2 == 0:
+            print('Error: Division by zero is not allowed.')
+            num2 = int(input('Enter the second number: '))
+        print(f'Result: {num1:.1f} / {num2:.1f} = {divide(num1, num2):.2f}')
+    elif operation == 5:
+        break
+
+    # Call function for play again
+    new_calc = ask_new_calculation()
+    if not new_calc:
+        break
+
+print_total_operations()
