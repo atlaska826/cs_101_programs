af0d353818d13c3c8482ac2c6db90043a81ea0ed
atlaska826 <131003995+atlaska826@users.noreply.github.com>
2023-09-20 15:57:22 -0500

    Finished adding comments to program2.py

--- Program 2/program2.py
+++ Program 2/program2.py
@@ -15,8 +15,6 @@ multiply_count = 0
 divide_count = 0
 
 """ FUNCTIONS """
-
-
 def print_display_menu():
     """Prints list of available operations with their corresponding selection number."""
     print('\n1. Addition')
@@ -29,7 +27,7 @@ def print_display_menu():
 def print_total_operations():
     """Computes and prints the total amount of operations. Also prints how many times each operation was used as long
     as it was used once."""
-    operation_count = add_count + subtract_count + multiply_count + divide_count
+    operation_count = add_count + subtract_count + multiply_count + divide_count # Adds up all operation totals
     print(f'\nYou had {operation_count} arithmetic operations as below:')
     if add_count > 0:
         print(f'"+" : {add_count}')
@@ -39,7 +37,7 @@ def print_total_operations():
         print(f'"*" : {multiply_count}')
     if divide_count > 0:
         print(f'"/" : {divide_count}')
-    if add_count == subtract_count == multiply_count == divide_count == 0:
+    if add_count == subtract_count == multiply_count == divide_count == 0:  # Runs only if no operations were performed
         print(f'No operations.')
 
     print('\nExiting the calculator. Goodbye!')
@@ -49,6 +47,7 @@ def ask_new_calculation():
     """Asks the user if they would like to perform another operation, and returns the corresponding boolean value."""
     perform_new_calc = input('\nDo you want to perform another calculation? (y/n): ').lower()
 
+    # Continues to prompt the user until y or n is entered
     while perform_new_calc != 'y' and perform_new_calc != 'n':
         perform_new_calc = input('Invalid choice, please select (y/n): ')
     if perform_new_calc == 'y':
@@ -61,6 +60,7 @@ def get_user_operation():
     """Allows the user to select which operation they want to perform."""
     print_display_menu()
     answer_choice = int(input('Please select an operation (1-5): '))
+    # Runs until the user enters a valid number
     while not (0 < answer_choice < 6):
         print('Invalid choice. Please select a valid operation.\n')
         answer_choice = int(input('Please select an operation (1-5): '))
@@ -68,19 +68,22 @@ def get_user_operation():
 
 
 def add(num1, num2):
-    """Adds the sum of FIXME num1 and num2"""
+    """Returns the sum of num1 and num2"""
     return num1 + num2
 
 
 def subtract(num1, num2):
+    """Returns the difference of num1 and num2"""
     return num1 - num2
 
 
 def multiply(num1, num2):
+    """Returns the product of num1 and num2"""
     return num1 * num2
 
 
 def divide(num1, num2):
+    """Returns the quotient of num1 divided by num2"""
     return num1 / num2
 
 
@@ -90,33 +93,37 @@ print('Welcome to the Simple Calculator!')
 while True:  # Loop runs until terminated by a break statement
     operation = get_user_operation()
 
-    if operation == 1:
+    # Calls functions based on which operation the user selected
+    if operation == 1:  # Add
         add_count += 1
         num1 = int(input('Enter the first number: '))
         num2 = int(input('Enter the second number: '))
         print(f'Result: {num1:.1f} + {num2:.1f} = {add(num1, num2):.2f}')
-    elif operation == 2:
+    elif operation == 2:  # Subtract
         subtract_count += 1
         num1 = int(input('Enter the first number: '))
         num2 = int(input('Enter the second number: '))
         print(f'Result: {num1:.1f} - {num2:.1f} = {subtract(num1, num2):.2f}')
-    elif operation == 3:
+    elif operation == 3:  # Multiply
         multiply_count += 1
         num1 = int(input('Enter the first number: '))
         num2 = int(input('Enter the second number: '))
         print(f'Result: {num1:.1f} * {num2:.1f} = {multiply(num1, num2):.2f}')
-    elif operation == 4:
+    elif operation == 4:  # Divide
         divide_count += 1
         num1 = int(input('Enter the first number: '))
         num2 = int(input('Enter the second number: '))
-        while num2 == 0:
+        while num2 == 0:  # Check if num2 would cause divide by zero error
             print('Error: Division by zero is not allowed.')
             num2 = int(input('Enter the second number: '))
         print(f'Result: {num1:.1f} / {num2:.1f} = {divide(num1, num2):.2f}')
-    elif operation == 5:
+    elif operation == 5:  # Exit
         break
 
+    # Calls the function to ask user if they would like to do another calculation
     new_calc = ask_new_calculation()
+
+    # Ends while loop if new_calc value is False
     if not new_calc:
         break
 
